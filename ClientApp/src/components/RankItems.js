f731195e7465654ca893373961590cc32d9b04b9
epiglotis <83986620+epiglotis@users.noreply.github.com>
2023-07-11 00:59:51 +0300

    ItemModel, ItemController, RankItems and Ranking Grid Integrated

---
+++ ClientApp/src/components/RankItems.js
@@ -0,0 +1,39 @@
+﻿import React, { useEffect, useState } from 'react';
+import MovieImageArr from './MovieImages';
+
+const RankItems = () => {
+  const [items, setItems] = useState([]);
+  const dataType = 1;
+
+  useEffect(() => {
+    fetch(`item/${dataType}`)
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
+
+  return (
+    <main>
+      <div className='items-not-ranked'>
+        {items.length > 0 ? (
+          items.map((item) => (
+            <div className='unranked-cell'>
+              <img
+                id={`item-${item.id}`}
+                src={MovieImageArr.find((o) => o.id === item.imageId)?.image}
+                alt={item.id}
+              />
+            </div>
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default RankItems;
